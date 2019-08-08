d329dcdf6de068566e88d51b99463726fbdd1d11
aniely.osilva <anielysilva08@gmail.com>
2019-08-08 17:09:43 -0300

    Initial commit

---
+++ app/src/main/java/com/example/atvpersonagens/Personagens_Activity.java
@@ -0,0 +1,14 @@
+package com.example.atvpersonagens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Personagens_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personagens_layout);
+    }
+}
---
+++ app/src/main/java/com/example/atvpersonagens/Splash_Activity.java
@@ -0,0 +1,25 @@
+package com.example.atvpersonagens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), Personagens_Activity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
