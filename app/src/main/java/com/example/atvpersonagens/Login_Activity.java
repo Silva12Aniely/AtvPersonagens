3eebd94a631a25ec2dc6b0316c571e699216e48d
aniely.osilva <anielysilva08@gmail.com>
2019-08-12 17:19:46 -0300

    DMC 08/08 Up senac

--- app/src/main/java/com/example/atvpersonagens/Login_Activity.java
+++ app/src/main/java/com/example/atvpersonagens/Login_Activity.java
@@ -4,15 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login_Activity extends AppCompatActivity {
+    Button entrar, sair;
+    EditText txtlogin, txtpass;
+
+    public static final String EXTRA_MESSAGE = "br.sp.senac.atvpersonagens.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
 
-        
+
+        txtlogin = (EditText) findViewById(R.id.txtlogin);
+        txtpass = (EditText) findViewById(R.id.txtpass);
+
     }
 
 
